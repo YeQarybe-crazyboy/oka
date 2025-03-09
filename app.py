f4cc714becd0f563418a711e2e6838da6a8bf241
YeQarybe-crazyboy <72519484+YeQarybe-crazyboy@users.noreply.github.com>
2025-03-09 11:20:28 +0330

    Add files via upload

---
+++ app.py
@@ -0,0 +1,19 @@
+from hashlib import md5
+from os import getenv
+from core import *
+
+@app.post('/')
+async def Bot(key: str, update: dict = Body(...,embed=False)):
+    if md5(key.encode()).hexdigest() != getenv('KEY'):
+        return
+    user, text, chat_type, message_id, utype = getInformation(update)
+    if utype != 'private':
+        return
+    
+    ch = list()
+    for channel in db['channels'] and user != ADMIN:
+        if not (await bot.isJoin(channel, user)):
+            ch.append(channel)
+    if ch:
+        await bot.SendMessage(user, getJoinText(ch), buttons['submit'], reply_message_id=message_id)
+        return
