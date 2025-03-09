454ccb0b6e2158a69faf461bd0d5543261f2abfd
YeQarybe-crazyboy <72519484+YeQarybe-crazyboy@users.noreply.github.com>
2025-03-09 12:29:52 +0330

    Add files via upload

--- app.py
+++ app.py
@@ -13,24 +13,25 @@ async def checkJoin(user):
 async def Bot(key: str, update: dict = Body(...,embed=False)):
     if md5(key.encode()).hexdigest() != getenv('KEY'):
         return
-    # user, text, chat_type, message_id, utype = getInformation(update)
-    await bot.SendMessage(7791061149, str(update))
-    # if chat_type != 'private':
-    #     return
+    user, text, chat_type, message_id, utype = getInformation(update)
 
-    # match utype:
-    #     case 1:
-    #         ch = await checkJoin(user)
-    #         if ch:
-    #             await bot.SendMessage(user, getJoinText(ch), buttons['submit'], reply_message_id=message_id)
-    #             return
-    #         if text == '/start':
-    #             await bot.SendMessage(user, ' ✅عضویت شما تایید شد.\n👇🏼 از دکمه های زیر استفاده کن', buttons['menu'], reply_message_id=message_id)
-    #     case 2:
-    #         if text == 'submit':
-    #             ch = await checkJoin(user)
-    #             if ch:
-    #                 await bot.sendRequest('answerCallbackQuery', {'callback_query_id': update['callback_query']['callback_query_id'], 'text': '❌ هنوز جوین نشدی'})
-    #                 return
-    #             else:
-    #                 await bot.EditMessage(user, ' ✅عضویت شما تایید شد.\n👇🏼 از دکمه های زیر استفاده کن', buttons['menu'])
+    if chat_type != 'private':
+        return
+
+    match utype:
+        case 1:
+            ch = await checkJoin(user)
+            if ch:
+                await bot.SendMessage(user, getJoinText(ch), buttons['submit'], reply_message_id=message_id)
+                return
+            if text == '/start':
+                await bot.SendMessage(user, ' ✅عضویت شما تایید شد.\n👇🏼 از دکمه های زیر استفاده کن', buttons['menu'], reply_message_id=message_id)
+        case 2:
+            await bot.SendMessage(user, str(update))
+            if text == 'submit':
+                ch = await checkJoin(user)
+                if ch:
+                    await bot.sendRequest('answerCallbackQuery', {'callback_query_id': update['callback_query']['id'], 'text': '❌ هنوز جوین نشدی'})
+                    return
+                else:
+                    await bot.EditMessage(user, ' ✅عضویت شما تایید شد.\n👇🏼 از دکمه های زیر استفاده کن', buttons['menu'])
