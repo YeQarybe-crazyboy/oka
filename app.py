a6def16157926d3254bbe3a2f5213e1280f17b13
YeQarybe-crazyboy <72519484+YeQarybe-crazyboy@users.noreply.github.com>
2025-03-09 15:33:13 +0330

    Add files via upload

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from hashlib import md5
 from core import *
 
-async def checkJoin(user):
+async def checkJoin(user, channels):
     ch = list()
-    for channel in db['channels']:
+    for channel in channels:
         if not (await bot.isJoin(channel, user)):
             ch.append(channel)
     return ch
@@ -13,21 +13,24 @@ async def Bot(key: str, update: dict = Body(...,embed=False)):
     if md5(key.encode()).hexdigest() != KEY:
         return
     user, text, chat_type, message_id, utype = getInformation(update)
+    channels = db['channels']
 
     if chat_type != 'private':
         return
 
     match utype:
         case 1:
-            ch = await checkJoin(user)
+            ch = await checkJoin(user, channels)
             if ch:
                 await bot.SendMessage(user, getJoinText(ch), buttons['submit'], reply_message_id=message_id)
                 return
             elif text == '/start':
                 await bot.SendMessage(user, ' ✅عضویت شما تایید شد.\n👇🏼 از دکمه های زیر استفاده کن', buttons['menu'], reply_message_id=message_id)
+            elif text.lower() in ('پنل', 'panel', '/panel') and user == ADMIN:
+                await bot.SendMessage(user, '⚙️ از دکمه های زیر برای مدیریت ربات استفاده کن', buttons['panel'], reply_message_id=message_id)
         case 2:
             if text == 'submit':
-                ch = await checkJoin(user)
+                ch = await checkJoin(user, channels)
                 if ch:
                     await bot.sendRequest('answerCallbackQuery', {'callback_query_id': update['callback_query']['id'], 'text': '❌ هنوز جوین نشدی'})
                     return
