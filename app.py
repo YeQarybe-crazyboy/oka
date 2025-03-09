1417e933399c13f4aca8aac8bc94d097e6b465ac
YeQarybe-crazyboy <72519484+YeQarybe-crazyboy@users.noreply.github.com>
2025-03-09 12:10:24 +0330

    Add files via upload

--- app.py
+++ app.py
@@ -2,21 +2,35 @@ from hashlib import md5
 from os import getenv
 from core import *
 
+async def checkJoin(user):
+    ch = list()
+    for channel in db['channels']:
+        if not (await bot.isJoin(channel, user)):
+            ch.append(channel)
+    return ch
+
 @app.post('/')
 async def Bot(key: str, update: dict = Body(...,embed=False)):
     if md5(key.encode()).hexdigest() != getenv('KEY'):
         return
     user, text, chat_type, message_id, utype = getInformation(update)
-    await bot.SendMessage(user, 'okaaaaaa')
     if chat_type != 'private':
         return
-    ch = list()
-    for channel in db['channels']:
-        await bot.SendMessage(user, (await bot.isJoin(channel, user)))
-        if not (await bot.isJoin(channel, user)):
-            ch.append(channel)
+
+    ch = await checkJoin(user)
     if ch:
         await bot.SendMessage(user, getJoinText(ch), buttons['submit'], reply_message_id=message_id)
         return
-    else:
-        await bot.SendMessage(user, 'ok')
+
+    match utype:
+        case 1:
+            if text == '/start':
+                await bot.SendMessage(user, ' ✅عضویت شما تایید شد.\n👇🏼 از دکمه های زیر استفاده کن', buttons['menu'], reply_message_id=message_id)
+        case 2:
+            if text == 'submit':
+                ch = await checkJoin(user)
+                if ch:
+                    await bot.sendRequest('answerCallbackQuery', {'callback_query_id': update['callback_query']['callback_query_id'], 'text': '❌ هنوز جوین نشدی'})
+                    return
+                else:
+                    await bot.EditMessage(user, ' ✅عضویت شما تایید شد.\n👇🏼 از دکمه های زیر استفاده کن', buttons['menu'])
